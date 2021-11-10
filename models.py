5637978dd14f9ccbb88dc741129360a83d7c472d
Chen Xu <chen.xu@crypto.com>
2021-11-10 18:36:01 +0800

    mod

--- models.py
+++ models.py
@@ -1,15 +1,31 @@
 import os, sys
 import numpy as np
 import pandas as pd
-from typing import Tuple, List, Dict, Type
+import matplotlib.pyplot as plt
+from typing import Tuple, List, Dict
+import bisect
 
 def linear_interpolation(
     timestamp: str,
     expiration: str,
     yields: List[float],
-    ttm: List[float]) -> Tuple(float, float):
-    pass
+    ttms: List[float]) -> Tuple[float, float]:
 
+    print(f"Current time: {timestamp}")
+    print(f"Expiration time: {expiration}")
+    print("Yields: ", yields)
+    print("TTMs: ", ttms)
+
+    deltaT = pd.to_datetime(expiration) - pd.to_datetime(timestamp)
+    ttm = (deltaT.days * SECONDS_IN_A_DAY + deltaT.seconds) / (365 * SECONDS_IN_A_DAY)
+
+    i = bisect.bisect_left(ttms, ttm)
+
+    return 0, 0
+
+
+TEST_CONTRACT = "2021-11-19 08:00:00"
+SECONDS_IN_A_DAY = 24 * 3600
 
 if __name__ == "__main__":
     print("Testing models.")
@@ -24,13 +40,24 @@ if __name__ == "__main__":
     df["days"] = (df["expiry"] - df["bar"]).dt.days
     df["seconds"] = (df["expiry"] - df["bar"]).dt.seconds
 
-    seconds_in_a_day = 24 * 3600
-    df["ttm"] = (df["days"] * seconds_in_a_day + df["seconds"]) / (365 * seconds_in_a_day)
+    df["ttm"] = (df["days"] * SECONDS_IN_A_DAY + df["seconds"]) / (365 * SECONDS_IN_A_DAY)
 
     df.set_index(["bar", "symbol"], inplace=True)
 
     bars = sorted(df.index.get_level_values(0).unique())
-    for bar in bars:
+    for bar in bars[:3]:
         df_tmp = df.xs(bar)
         df_tmp.sort_values("ttm", inplace=True)
-        print(df_tmp)
+
+        fut_prc = (df_tmp["a1"] + df_tmp["b1"]) / 2.0
+        spot_prc = (df_tmp["a1_spot"] + df_tmp["b1_spot"]) / 2.0
+#        yields = list((fut_prc.div(spot_prc) - 1.0).div(df_tmp["ttm"]).values)
+        yields = list((fut_prc.div(spot_prc) - 1.0).values)
+
+        ttms = list(df_tmp["ttm"].values)
+
+        yld, ttm = linear_interpolation(
+            bar.strftime("%Y-%m-%d %H:%M:%S"),
+            TEST_CONTRACT,
+            yields,
+            ttms)
