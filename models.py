6027a1b84fb9822fbb0aa5f5bb4cb483ef7e1905
Chen Xu <chen.xu@crypto.com>
2021-11-10 21:54:40 +0800

    mod

--- models.py
+++ models.py
@@ -8,20 +8,49 @@ import bisect
 def linear_interpolation(
     timestamp: str,
     expiration: str,
-    yields: List[float],
-    ttms: List[float]) -> Tuple[float, float]:
+    contracts: List[str],
+    ttms: List[float],
+    yields: List[float]) -> Tuple[float, float]:
 
-    print(f"Current time: {timestamp}")
-    print(f"Expiration time: {expiration}")
-    print("Yields: ", yields)
-    print("TTMs: ", ttms)
+    if not yields or not ttms:
+        print("Empty data!")
+        return 0, 0
+
+    # print(f"Current time: {timestamp}")
+    # print(f"Expiration time: {expiration}")
+    # print("Contracts: ", contracts)
+    # print("TTMs: ", ttms)
+    # print("Yields: ", yields)
 
     deltaT = pd.to_datetime(expiration) - pd.to_datetime(timestamp)
     ttm = (deltaT.days * SECONDS_IN_A_DAY + deltaT.seconds) / (365 * SECONDS_IN_A_DAY)
 
     i = bisect.bisect_left(ttms, ttm)
+    if 0 < i < len(yields):
+        print(f"Using contracts: {contracts[i - 1]} {contracts[i]}")
+        x1, y1 = ttms[i - 1], yields[i - 1]
+        x2, y2 = ttms[i], yields[i]
+    elif i == 0:
+        print(f"Using spot and contracts {contracts[i]}")
+        x1, y1 = 0, 0
+        x2, y2 = ttms[i], yields[i]
+    elif i == len(yields):
+        print(f"Using contracts: {contracts[i - 2]} {contracts[i - 1]}")
+        x1, y1 = 0, 0
+        if i > 1:
+            x1, y1 = ttms[i - 2], yields[i - 2]
+            x2, y2 = ttms[i - 1], yields[i - 1]
+    else:
+        print(f"Invalid futures yield curve, i = {i}")
+        return 0, 0
 
-    return 0, 0
+    try:
+        slope = (y2 - y1) / (x2 - x1)
+        y = y1 + (ttm - x1) * slope
+    except Exception as e:
+        print(f"Couldn't do interpolation using x1={x1}, y1={y1}, x2={x2}, y2={y2}")
+        
+    return y, ttm
 
 
 TEST_CONTRACT = "2021-11-19 08:00:00"
@@ -45,7 +74,7 @@ if __name__ == "__main__":
     df.set_index(["bar", "symbol"], inplace=True)
 
     bars = sorted(df.index.get_level_values(0).unique())
-    for bar in bars[:3]:
+    for bar in bars:
         df_tmp = df.xs(bar)
         df_tmp.sort_values("ttm", inplace=True)
 
@@ -56,8 +85,13 @@ if __name__ == "__main__":
 
         ttms = list(df_tmp["ttm"].values)
 
+        contracts = list(df_tmp.index)
+
         yld, ttm = linear_interpolation(
             bar.strftime("%Y-%m-%d %H:%M:%S"),
             TEST_CONTRACT,
-            yields,
-            ttms)
+            contracts,
+            ttms,
+            yields)
+
+        print(bar, yld, ttm)
